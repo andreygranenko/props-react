40952bd3dcc78a110fa8e4a95876fdad4433713a
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2023-11-26 22:19:14 +0200

    for my cooperate using

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+
+
+class WhoAmI extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      text: '+++',
+      position: ''
+    };
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+        years: state.years + 1
+    }))
+  }
+
+  commitInputChanges = (e) => {
+      this.setState({
+          position: e.target.value
+      })
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    const {position, years, text} = this.state
+    return (
+      <div className='main'>
+        <h1>My name is <span>{name}</span>, <br/>
+            surname - <span>{surname}</span>, <br/>
+            age - <span>{years}</span>,
+            position - <span>{position}</span></h1>
+        <a id={'link'} href={link}>My profile</a>
+        <button onClick={this.nextYear}>{text}</button>
+        <form action="">
+            <span>Введите должность</span>
+            <input type="text" onChange={this.commitInputChanges}/>
+        </form>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WhoAmI name='Andrew' surname="Granenko" link="facebook.com"/>
+      <WhoAmI name='Alex' surname="Vodnev" link="vk.com"/>
     </div>
   );
 }
