c75eb38cad1960cd36111237682a7c190cb9e4b3
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2023-12-19 22:05:23 +0200

    tested bootstrap react, created form and carousel using it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import { Component} from 'react';
+import {styled} from "styled-components";
 import './App.css';
 
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, .2);
+  a {
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${props => props.active ? 'orange' : 'black'};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`;
 
+const Header = styled.h2`
+  font-size: 22px;
+`;
+
+export const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0, 0, 0, .2);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, .2);
+`;
 
 class WhoAmI extends Component {
 
@@ -31,12 +58,12 @@ class WhoAmI extends Component {
     const {position, years, text} = this.state
     return (
       <div className='main'>
-        <h1>My name is <span>{name}</span>, <br/>
+        <Header>My name is <span>{name}</span>, <br/>
             surname - <span>{surname}</span>, <br/>
             age - <span>{years}</span>,
-            position - <span>{position}</span></h1>
-        <a id={'link'} href={link}>My profile</a>
-        <button onClick={this.nextYear}>{text}</button>
+            position - <span>{position}</span></Header>
+        <a href={link}>My profile</a>
+        <Button onClick={this.nextYear}>{text}</Button>
         <form action="">
             <span>Введите должность</span>
             <input type="text" onChange={this.commitInputChanges}/>
@@ -46,12 +73,18 @@ class WhoAmI extends Component {
   }
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
 function App() {
   return (
-    <div className="App">
-      <WhoAmI name='Andrew' surname="Granenko" link="facebook.com"/>
-      <WhoAmI name='Alex' surname="Vodnev" link="vk.com"/>
-    </div>
+      <EmpItem active>
+        <div className="App">
+          <WhoAmI name='Andrew' surname="Granenko" link="facebook.com"/>
+          <WhoAmI name='Alex' surname="Vodnev" link="vk.com"/>
+        </div>
+      </EmpItem>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Button} from "./App";
+import BootstrapTest from './BootstrapTest';
+import {styled} from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const BigButton = styled(Button)`
+  margin: 0 auto;
+  width: 245px;
+  text-align: center;
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <BigButton as='a'>Отправить отчет</BigButton>
+    <BootstrapTest/>
   </React.StrictMode>
 );
