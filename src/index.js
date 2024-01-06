7fed96d41a9ba655e453dbbd2cc7951ce02c408f
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-01-06 20:17:52 +0200

    added portals

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import {Button} from "./App";
 import {styled} from "styled-components";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App2 from "./App2";
+import App3 from "./App3";
 
 const BigButton = styled(Button)`
   margin: 0 auto;
@@ -15,7 +16,7 @@ const BigButton = styled(Button)`
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App3/>
     <BigButton as='a'>Отправить отчет</BigButton>
   </React.StrictMode>
 );
